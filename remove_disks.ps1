d6e99eacf4a43b9d243ca295a94ab5e7ebef1db5
stilltum <35137975+stilltum@users.noreply.github.com>
2024-06-15 17:07:07 +0800

    Update remove_disks.ps1

--- remove_disks.ps1
+++ remove_disks.ps1
@@ -1,3 +1,4 @@
+$debugging = $true
 # Filter and output disk drives connected via USB and internally
 Get-PnpDevice | Where-Object { $_.Class -eq "DiskDrive" } | ForEach-Object {
     $diskDrive = $_
@@ -9,8 +10,16 @@ Get-PnpDevice | Where-Object { $_.Class -eq "DiskDrive" } | ForEach-Object {
         $ParentClass = $parentDevice.Class
     }
 
-    # True deletes device DiskDrive USB devices that aren't currently connected
-    if ($false) {
+    # Outputs all DiskDrives (including non-usb devices) currently on the system
+    if ($debugging) {
+            [PSCustomObject]@{
+            DeviceID = $deviceID
+            Description = $diskDrive.Description
+            ParentClass = $ParentClass
+            Present = $diskDrive.Present
+        }
+    # Deletes device DiskDrive USB devices that aren't currently connected
+    } else {
         if ($ParentClass -eq "USB" -and $diskDrive.Present -eq $false) {
             Write-Host "Deleting "$deviceID" Parent: "$ParentClass
             $command = "pnputil /remove-device '" + $deviceID + "'; Exit"
@@ -18,13 +27,5 @@ Get-PnpDevice | Where-Object { $_.Class -eq "DiskDrive" } | ForEach-Object {
         } else {
             Write-Host "Keeping "$deviceID" Parent: "$ParentClass
         }
-    # False outputs all DiskDrives (including non-usb devices) currently on the system
-    } else {
-        [PSCustomObject]@{
-            DeviceID = $deviceID
-            Description = $diskDrive.Description
-            ParentClass = $ParentClass
-            Present = $diskDrive.Present
-        }
-    }
+    } 
 }
